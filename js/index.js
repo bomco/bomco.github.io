46c9bb90668ac917d02bb95c342e0754a5d53b9e
봄코딩 <bomcoding@gmail.com>
2019-04-06 20:19:37 +0900

    내용 추가

--- js/index.js
+++ js/index.js
@@ -137,18 +137,120 @@ window.addEventListener('load', function(){
 
 /***********************Particles*************/
 
-particlesJS("particles-js", {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},
-"color":{"value":"#fff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
-"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
-"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
-"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
-"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
-var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() { stats.begin(); stats.end();
-if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-  count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; }
-requestAnimationFrame(update); }; requestAnimationFrame(update);;
+// particlesJS("particles-js", {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},
+// "color":{"value":"#E5554F"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
+// "opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
+// "line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+// "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
+// "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+// var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute';
+// stats.domElement.style.left = '0px';
+// stats.domElement.style.top = '0px';
+// document.body.appendChild(stats.domElement);
+// count_particles = document.querySelector('.js-count-particles');
+// update = function() { stats.begin(); stats.end();
+// if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+//   count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; }
+// requestAnimationFrame(update); }; requestAnimationFrame(update);;
+
+var stageWrap = document.getElementById('petal-stage-wrap');
+var stage = document.getElementById('petal-stage');
+var stageWidth  = stageWrap.clientWidth;
+var stageHeight = stageWrap.clientHeight;
+var petalNum = 30;
+var petalObjArr = [];
+var g = 0.00004;
+var w = 0;
+var wDeg = 0;
+var fps = 60;
+var frameTime = 1000 / fps;
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getDegree = function(radian) {
+  return radian / Math.PI * 180;
+};
+
+var getRadian = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+var petal = function(num) {
+  this.elm  = document.createElement('div');
+  this.size = 's';
+  this.t    = 0;
+  this.x    = 0;
+  this.vx   = 0;
+  this.py   = 0;
+  this.y    = 0;
+  this.rotateY = 0;
+  this.rotateYUnit = getRandomInt(10, 50) / frameTime;
+  this.rotateZ = getRandomInt(-45, 45);
+  
+  if (num % 3 == 1) this.size = 'm';
+  if (num % 3 == 2) this.size = 'l';
+  
+  this.elm.id = 'petal-' + num;
+  this.elm.className = 'petal-model petal-model-' + this.size;
+  this.elm.innerHTML = '<img src="http://www.tplh.net/file/petal.png" alt="" />';
+};
+
+petal.prototype.init = function() {
+  var deg = getRandomInt(0, 90);
+  this.t  = 0;
+  this.x  = getRandomInt(100, 1000) * -1;
+  this.vx = getRandomInt(1, 5);
+  this.py = getRandomInt(-100, stageHeight / 2);
+};
+
+petal.prototype.build = function() {
+  stage.appendChild(this.elm);
+  this.init();
+};
+
+petal.prototype.move = function() {
+  this.t += frameTime;
+  this.x += this.vx + w;
+  this.y = 1 / 2 * g * this.t * this.t + this.py;
+  this.elm.style.top = this.y + 'px';
+  this.elm.style.left = this.x + 'px';
+};
+
+petal.prototype.rotate = function() {
+  this.rotateY += this.rotateYUnit;
+  this.elm.style.transform = 'rotateZ(' + this.rotateZ + 'deg) rotateY(' + this.rotateY + 'deg)';
+};
+
+petal.prototype.reset = function() {
+  if (this.x < stageWidth && this.y < stageHeight) return;
+  this.init();
+};
+
+var petalObjRender = function() {
+  for (var i = 0; i < petalObjArr.length; i++) {
+    petalObjArr[i].move();
+    petalObjArr[i].rotate();
+    petalObjArr[i].reset();
+  }
+};
+
+var blowWind = function() {
+  wDeg += 0.2;
+  w = Math.pow(Math.sin(getRadian(wDeg)) + 1, 2);
+};
+
+var init = function() {
+  for (var i = 0; i < petalNum; i++) {
+    petalObjArr[i] = new petal(i);
+    petalObjArr[i].build();
+  }
+  
+  setInterval(function() {
+    petalObjRender();
+    blowWind();
+  }, frameTime);
+};
+
+init();
